07a839dfa5cc00c5c9cb4a6884eb8b1369614376
Olivier Mangin <olivier.mangin@inria.fr>
2013-10-18 20:40:19 +0200

    Better association system to generate multimodal data.

---
+++ pairing.py
@@ -0,0 +1,66 @@
+import random
+
+from multimodal.db.acorns import load as load_acorns
+from multimodal.db.choreo2 import load as load_choreo
+
+
+def named_labels_to_range(sample_labels, shuffle=False):
+    names = list(set(sample_labels))
+    if shuffle:
+        random.shuffle(names)
+    inversion = dict((n, i) for i, n in enumerate(names))
+    return names, [inversion[n] for n in sample_labels]
+
+
+def organize_by_values(l, nb_values):
+    buckets = [[] for i in range(nb_values)]
+    for i, x in enumerate(l):
+        buckets[x].append(i)
+    return buckets
+
+
+def associate(l):
+    # buckets_tuple: one bucket for each modality grouped in a tuple
+    return [zip(*buckets_tuple) for buckets_tuple in zip(*l)]
+
+
+def flatten(l):
+    return sum(l, [])
+
+
+def associate_labels(labels_by_modality, shuffle=False):
+    """ labels_by_modality: for each modality, list of labels for each sample.
+        suffle: shuffle label associations (the association between samples is
+                always shuffled).
+    """
+    # Replace label names by index in range
+    names, new_labels = zip(*[named_labels_to_range(l)
+                              for l in labels_by_modality])
+    # Ensure all modalities have same number of labels
+    n_labels = len(names[0])
+    assert(all([len(l) == n_labels for l in names]))
+    # Organize labels
+    by_labels = [organize_by_values(l, n_labels) for l in new_labels]
+    # Shuffle associations
+    for l_mod in by_labels:
+        for l in l_mod:
+            random.shuffle(l)
+    # Associate
+    associated = associate(by_labels)
+    new_labels = [[i] * len(l) for i, l in enumerate(associated)]
+    return names, flatten(new_labels), flatten(associated)
+
+
+def get_sound_labels(speaker):
+    db = load_acorns(1, blacklist=True)
+    return [db.tags[r.tags[0]] for r in db.records[speaker]]
+
+
+def get_motion_labels():
+    motion_db = load_choreo(verbose=False)
+    motion_names = motion_db.label_descriptions
+    return [motion_names[r[1][0]] for r in motion_db.records]
+
+
+def associate_sound_motion(speaker):
+    return associate_labels([get_sound_labels(speaker), get_motion_labels()])
---
+++ test/test_pairing.py
@@ -0,0 +1,70 @@
+from unittest import TestCase, skip
+
+
+from multimodal.pairing import (associate, flatten, organize_by_values,
+                                associate_labels, associate_sound_motion,
+                                get_sound_labels, get_motion_labels)
+
+
+class TestVar(TestCase):
+
+    def test_associate(self):
+        l = [[[1, 2], [3, 4]],
+             [['a', 'b', 'c'], [',', '?']],
+             [[0.3, 0.4], [0.4, 0.2]],
+             ]
+        assoc = [[(1, 'a', .3), (2, 'b', .4)], [(3, ',', .4), (4, '?', .2)]]
+        self.assertEqual(associate(l), assoc)
+
+    def test_flatten(self):
+        l = [range(4), range(4, 10), range(10, 15)]
+        self.assertEqual(flatten(l), range(15))
+
+    def test_organize_by_values(self):
+        l = [0, 0, 1, 1, 0, 2, 1]
+        org = [[0, 1, 4], [2, 3, 6], [5]]
+        self.assertEqual(organize_by_values(l, 3), org)
+
+
+class TestModalityAssociation(TestCase):
+
+    def setUp(self):
+        self.sets = [[1, 1, 2, 1, 3, 2, 3], ['4', '5', '6', '5', '5', '4']]
+
+    def test_fails_on_different_nb_of_labels(self):
+        self.sets[0].append(0)
+        with self.assertRaises(AssertionError):
+            associate_labels(self.sets)
+
+    def test_names(self):
+        names, _dum, _my = associate_labels(self.sets)
+        self.assertEqual(set(names[0]), set(range(1, 4)))
+        self.assertEqual(set(names[1]), set(['4', '5', '6']))
+
+    def test_labels_match_between(self):
+        names, _dummy, assocs = associate_labels(self.sets)
+        self.assertEqual([names[0].index(self.sets[0][x[0]]) for x in assocs],
+                         [names[1].index(self.sets[1][x[1]]) for x in assocs])
+
+    def test_labels_match_origin(self):
+        names, labels, assocs = associate_labels(self.sets)
+        self.assertEqual([names[0][l] for l in labels],
+                         [self.sets[0][x[0]] for x in assocs])
+        self.assertEqual([names[1][l] for l in labels],
+                         [self.sets[1][x[1]] for x in assocs])
+
+    @skip("Too long to run each time but good to have!")
+    def test_labels_match_on_db(self):
+        sound_labels = get_sound_labels(1)
+        motion_labels = get_motion_labels()
+        names, labels, tuples = associate_sound_motion(1)
+        sound_names = names[0]
+        sound_idx = [t[0] for t in tuples]
+        motion_names = names[1]
+        motion_idx = [t[1] for t in tuples]
+        # Check sound output labels
+        self.assertEqual([sound_names[l] for l in labels],
+                         [sound_labels[i] for i in sound_idx])
+        # Check motion output labels
+        self.assertEqual([motion_names[l] for l in labels],
+                         [motion_labels[i] for i in motion_idx])
