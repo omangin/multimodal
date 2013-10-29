bdf803e686b0e791a758ad00e237fbe166bb382f
Olivier Mangin <olivier.mangin@inria.fr>
2013-10-29 15:53:39 +0100

    Remove unused imports from db/__init__.py.

--- db/__init__.py
+++ db/__init__.py
@@ -3,15 +3,10 @@
 The following databases are available:
     - Choreography v1:
         -> module: choreo1
-        -> loader: load_choreo1
 
     - Choreography v2:
         -> module: choreo2
-        -> loader: load_choreo2
 
-    - Acorns:
-        ...
+    - Acorns Caregiver:
+        -> module: acorns
 """
-
-from .choreo1 import load as load_choreo1
-from .choreo2 import load as load_choreo2
