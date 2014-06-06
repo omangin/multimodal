a446db29487a19238d6dbdd165e4a629ba8af3b3
Olivier Mangin <olivier.mangin@inria.fr>
2014-06-06 12:37:31 +0200

    Implements incremental kmeans as in Driesen2012 for HACs.
    Also makes function to build codebooks from wav more memory efficient.
    Fix unseeded test for Kullback Leibler.

--- features/hac.py
+++ features/hac.py
@@ -7,10 +7,13 @@ from librosa.filters import dct
 
 
 """Implementation of the histograms of acoustic co-occurrences (HAC)
-from [VanHamme2008].
+from [VanHamme2008] and [Driesen2012].
 
 .. [VanHamme2008] Van Hamme, Hugo "HAC-models: a Novel Approach to Continuous
                   Speech Recognition", Interspeech ISCA (2008)
+.. [Driesen2012] Driesen, Joris "Discovering words in speech using matrix
+                 factorization", PhD thesis (2012)
+               
 """
 
 
@@ -57,12 +60,49 @@ def mfcc(data, sr=22050, n_mfcc=20, **kwargs):
     return np.dot(dct(n_mfcc, S.shape[0]), S)
 
 
-def build_codebook(data, k):
-    codebook, _ = kmeans2(data, k)
+def iterative_kmeans(data, k, alpha=0.01):
+    """Iterative kmeans: split each cluster in two along principal direction
+    and run kmeans, until k is reached. (See [Driesen2012], Alg. 1.1, p53)
+
+    params:
+        - k: int
+            number of clusters
+        - alpha: double
+            distance of split from original centroid
+    """
+    centroids = data.mean(axis=0)[np.newaxis, :]
+    while centroids.shape[0] < k:
+        quantized, _ = vq(data, centroids)
+        counts = np.bincount(quantized, minlength=centroids.shape[0])
+        has_max_points = counts.argmax()
+        # Get svd for points associated
+        associated = (quantized == has_max_points).nonzero()[0]
+        u, s, v = np.linalg.svd(data[quantized, :])
+        p = v[0, :] * np.sqrt(s[0])
+        # Split centroid
+        p1 = centroids[has_max_points] + alpha * p
+        p2 = centroids[has_max_points] - alpha * p
+        centroids = np.vstack([centroids[:has_max_points, :],
+                               centroids[has_max_points + 1:, :],
+                               p1,
+                               p2,
+                               ])
+        # Run kmeans
+        centroids, _ = kmeans2(data, centroids, minit='matrix')
+    return centroids
+
+
+def build_codebook(data, k, mode='raw'):
+    if mode == 'raw':
+        codebook, _ = kmeans2(data, k)
+    elif mode == 'iterative':
+        codebook = iterative_kmeans(data, k)
+    else:
+        raise(ValueError('Invalid mode, should be raw or iterative'))
     return codebook
 
 
-def build_codebooks_from_list_of_wav(wavs, ks):
+def build_codebooks_from_list_of_wav(wavs, ks, mode='raw'):
     """Generates three codebooks of low level units from a list of wav files.
 
     The three codebooks corresponds to a quantization of MFCC vectors
@@ -77,19 +117,26 @@ def build_codebooks_from_list_of_wav(wavs, ks):
         triple of codebooks as (k, d) arrays
     """
     mfccs = []
-    d_mfccs = []
-    dd_mfccs = []
     for w in wavs:
         print("preprocessing {}".format(w))
         sr, data = wavfile.read(w)
         cur_mfccs = mfcc(data, sr=sr, **MFCC_PARAMS)
-        mfccs.append(cur_mfccs.T)
-        d_mfccs.append(delta(cur_mfccs).T)
-        dd_mfccs.append(delta(cur_mfccs, order=2).T)
-    print("Building codebooks...")
-    return (build_codebook(np.vstack(mfccs), ks[0]),
-            build_codebook(np.vstack(d_mfccs), ks[1]),
-            build_codebook(np.vstack(dd_mfccs), ks[2]))
+        mfccs.append(cur_mfccs)
+        #mfccs.append(cur_mfccs.T)
+        #d_mfccs.append(delta(cur_mfccs).T)
+        #dd_mfccs.append(delta(cur_mfccs, order=2).T)
+    print("Building codebooks:")
+    print("- MFCC...")
+    cdb_mfcc = build_codebook(np.vstack([m.T for m in mfccs]), ks[0], mode=mode)
+                              ks[0], mode=mode)
+    print("- Delta MFCC...")
+    cdb_dmfcc = build_codebook(np.vstack([delta(m).T for m in mfccs]), ks[1], mode=mode)
+                               ks[1], mode=mode)
+    print("- Delta Delta MFCC...")
+    cdb_ddmfcc = build_codebook(np.vstack([delta(m, order=2).T for m in mfccs]), ks[2], mode=mode)
+            np.vstack([delta(m, order=2).T for m in mfccs]),
+            ks[2], mode=mode)
+    return (cdb_mfcc, cdb_dmfcc, cdb_ddmfcc)
 
 
 def coocurrences(quantized_data, n_quantized, lag):
--- test/test_hac.py
+++ test/test_hac.py
@@ -1,8 +1,9 @@
 import unittest
 
 import numpy as np
+from scipy.cluster.vq import kmeans2
 
-from multimodal.features.hac import coocurrences
+from multimodal.features.hac import coocurrences, iterative_kmeans
 
 
 class TestCoocurrences(unittest.TestCase):
@@ -24,3 +25,45 @@ class TestCoocurrences(unittest.TestCase):
         result = coocurrences(quantized, 2, lag=2)
         counts = [0, 1, 2, 2]
         self.assertEquals(list(result), counts)
+
+
+class TestIterativeKmeans(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(0)
+        self.cent = [np.array([0, 1]), np.array([0, -1]), np.array([10, 0])]
+        self.data = np.vstack([
+            np.random.multivariate_normal(c, .5 * np.eye(2), 10)
+            for c in self.cent])
+        np.random.shuffle(self.data)
+
+    def test_shape_is_ok(self):
+        data = np.random.random((20, 3))
+        centroids = iterative_kmeans(data, 5)
+        self.assertEquals(centroids.shape, (5, 3))
+
+    def test_is_converged(self):
+        data = np.random.random((20, 3))
+        centroids = iterative_kmeans(data, 5)
+        centroids2, _ = kmeans2(data, centroids, minit='matrix')
+        np.testing.assert_allclose(centroids, centroids2)
+
+    def test_ex_k2(self):
+        centroids = iterative_kmeans(self.data, 2)
+        self.assertTrue(
+            np.allclose(centroids, np.array([[0, 0], [10, 0]]), atol=1)
+            or np.allclose(centroids, np.array([[10, 0], [0, 0]]), atol=1)
+            )
+
+    def test_ex_k3(self):
+        centroids = iterative_kmeans(self.data, 3)
+        self.assertTrue(
+            np.allclose(centroids,
+                        np.array([[0, 1], [0, -1], [10, 0]]), atol=1)
+            or np.allclose(centroids,
+                           np.array([[0, 1], [0, -1], [10, 0]]), atol=1)
+            or np.allclose(centroids,
+                           np.array([[10, 0], [0, -1], [0, 1]]), atol=1)
+            or np.allclose(centroids,
+                           np.array([[10, 0], [0, 1], [0, -1]]), atol=1)
+            )
